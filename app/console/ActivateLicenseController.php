8778ca3f7f1e8727c6d3783b50cf46046cc78920
carson <docxcn@gmail.com>
2017-09-19 16:42:22 +0800

    更新console模块

---
+++ app/console/ActivateLicenseController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\console;
+
+use app\api\models\v2\License;
+use yii\console\Controller;
+
+class ActivateLicenseController extends Controller
+{
+
+    protected $signature = 'activate:license {server} {code}';
+
+    protected $description = 'Activate License By Code';
+
+    public function handle()
+    {
+        $server = $this->argument('server');
+        $code = $this->argument('code');
+
+        $res = curl_request("{$server}/v1/licenses/activate", 'POST', ['code' => $code]);
+
+        if (isset($res['success']) && $res['success'] == false) {
+            echo '授权失败';
+            exit;
+        }
+
+        if ($model = License::first()) {
+            $model->delete();
+        }
+
+        License::create($res['data']);
+
+        $this->call('cache:clear');
+
+        echo "授权成功\n\n";
+    }
+}
---
+++ app/console/RebuildController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace app\console;
+
+use yii\console\Controller;
+
+class RebuildController extends Controller
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:rebuild';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'rebuild the application';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $app_path = app_path();
+
+        // 更新 Libraries 文件
+        $list = glob($app_path . '/libraries/*');
+        foreach ($list as $vo) {
+            $name = str_replace('cls_', '', basename($vo, '.php'));
+            rename($vo, dirname($vo) . '/' . camel_case($name, true) . '.php');
+        }
+
+        // 更新 Helpers 文件
+        $frontend = glob($app_path . '/helpers/*');
+        $backend = glob($app_path . '/modules/*/helpers/*');
+        $list = array_merge($frontend, $backend);
+        foreach ($list as $vo) {
+            $name = str_replace('lib_', '', basename($vo, '.php'));
+            rename($vo, dirname($vo) . '/' . $name . '.php');
+        }
+
+        // 更新 Controller 文件
+        $frontend = glob($app_path . '/http/controllers/*');
+        $backend = glob($app_path . '/modules/*/controllers/*');
+        $list = array_merge($frontend, $backend);
+        foreach ($list as $vo) {
+            $name = basename($vo, '.php');
+            if ($name != 'Controller') {
+                rename($vo, dirname($vo) . '/' . camel_case($name, true) . 'Controller.php');
+            }
+        }
+    }
+
+    public function actionMigration()
+    {
+        $database_path = database_path();
+
+        $list = glob($database_path . '/migrations/*');
+
+        foreach ($list as $vo) {
+            $content = file_get_contents($vo);
+            $content = str_replace('ecs_', '', $content);
+            file_put_contents($vo, $content);
+
+            $name = str_replace('ecs_', '', basename($vo, '.php'));
+            rename($vo, dirname($vo) . '/' . $name . '.php');
+        }
+    }
+}
